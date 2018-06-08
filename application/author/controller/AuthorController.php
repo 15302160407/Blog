e3bdb5d9f6ea933758dc5471ebb44e28f353b1e6
何素薇 <1421297018@qq.com>
2018-06-08 17:07:48 +0800

    2018.6.8

--- application/author/controller/AuthorController.php
+++ application/author/controller/AuthorController.php
@@ -3,28 +3,28 @@ namespace app\author\controller;
 use think\Controller;
 class AuthorController extends Controller//登录
 {
-    public function index()
-    {
-        $auhtorId=session("authorId",'',"id");
-        $myDetail=model("Author")->getAuthor($auhtorId);
-        $this->assign('myDetail',$myDetail);
-        return $this->fetch();
-    }
+  public function index()
+  {
+    $auhtorId=session("authorId",'',"id");
+    $myDetail=model("Author")->getAuthor($auhtorId);
+    $this->assign('myDetail',$myDetail);
+    return $this->fetch();
+  }
 
 //注册
-    public function register(){
-        return $this->fetch();
-    }
+  public function register(){
+    return $this->fetch();
+  }
 
 //保存注册信息
-    public function save(){
-      if(!request()->isPost()){
-          $this->error("非法提交！");
-      }
-      $data=input('post.');
-      $validate=validate('Author');
-      if(!$validate->check($data)){
-        $this->error($validate->getError());
+  public function save(){
+    if(!request()->isPost()){
+      $this->error("非法提交！");
+    }
+    $data=input('post.');
+    $validate=validate('Author');
+    if(!$validate->check($data)){
+      $this->error($validate->getError());
     }
     $authorData=[
     'username'=>$data['username'],
@@ -40,17 +40,17 @@ class AuthorController extends Controller//登录
     $authorId=model('Author')->add($authorData);
     session("authorId",$authorId,"id");
     if($authorId){
-        $this->success("注册成功！","author/index");
+      $this->success("注册成功！","author/index");
     }
     else{
-        $this->error("增加数据失败");
+      $this->error("增加数据失败");
     }
-}
+  }
 
-public function ajaxUpload(){
+  public function ajaxUpload(){
         $file = $this->request->file('file');//file是传文件的名称，这是webloader插件固定写入的。因为webloader插件会写入一个隐藏input，不信你们可以通过浏览器检查页面
-        $info = $file->move(ROOT_PATH . 'public/static' . DS . 'authorLogo','');
-    }
+        $info = $file->move(ROOT_PATH . 'public/static/images' . DS . 'authorLogo','');
+      }
 
 
 
@@ -68,34 +68,55 @@ public function ajaxUpload(){
         $auhtorId=session("authorId",'',"id");
         $myDetail=model("Author")->getAuthor($auhtorId);
         $this->assign('myDetail',$myDetail);
-        return $this->fetch();
+       return $this->fetch();
     }
     public function update(){
-      if(!request()->isPost()){
-       $this->error("非法提交！");
-   }
-   $data=input('post.');
-   $validate=validate('Author');
-   if(!$validate->check($data)){
-       $this->error($validate->getError());
-   }
-   $authorData=[
-   'username'=>$data['username'],
-   'realname'=>$data['realname'],
-   'password'=>$data['password'],
-   'code'=>$data['code'],
-   'tel'=>$data['tel'],
-   'email'=>$data['email'],
-   'note'=>$data['note'],
-   'update'=>time()
-   ];
-   $authorId=model('Author')->save($authorData,['id'=>intval($data['id'])]);
-   if($authorId){
-       $this->success("修改数据成功！","author/detail");
-   }
-   else{
-       $this->error("修改数据失败！");
-   }
-}
+        if(!request()->isPost()){
+         $this->error("非法提交！");
+      }
+       $data=input('post.');
+       $validate=validate('Author');
+       if(!$validate->check($data)){
+         $this->error($validate->getError());
+       }
+       $authorData=[
+       'username'=>$data['username'],
+       'realname'=>$data['realname'],
+       'password'=>$data['password'],
+       'code'=>$data['code'],
+       'tel'=>$data['tel'],
+       'email'=>$data['email'],
+       'note'=>$data['note'],
+       'update_time'=>time()
+       ];
 
-}
+
+
+    // 获取表单上传文件 例如上传了001.jpg
+    $file = request()->file('image');
+    
+    // 移动到框架应用根目录/public/uploads/ 目录下
+    if($file){
+        $info = $file->move(ROOT_PATH . 'public' . DS . 'uploads','');
+        if($info){
+            // 成功上传后 获取上传信息
+            // 输出 20160820/42a79759f284b767dfcb2a0197904287.jpg
+            // $this->success("上传图片成功！",'index/index');
+        }else{
+            // 上传失败获取错误信息
+            echo $file->getError();
+        }
+    }
+
+
+       $authorId=model('Author')->save($authorData,['id'=>intval($data['id'])]);
+       if($authorId){
+         $this->success("修改数据成功！","author/detail");
+       }
+       else{
+         $this->error("修改数据失败！");
+       }
+
+     }
+
+   }
